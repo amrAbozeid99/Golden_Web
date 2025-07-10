385299eb70f1adfa872dfb7191317365dc272804
amr apozeid <x.genral990@gmail.com>
2025-07-10 10:30:42 +0300

    update 10-7-2025
    all working naw

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,6 +1,9 @@
 "use client";
 import React from 'react'
 import Link from 'next/link';
+import  { useRef } from 'react';
+import { useState , useEffect } from 'react';
+import emailjs from '@emailjs/browser';
 import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -26,6 +29,40 @@ import { FaApple } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 
 const Contact = () => {
+
+
+
+  // كود التشغيل 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_al1d1ei', 'template_qa0qhlb', form.current, {
+        publicKey: 'AVv7bM7Scd-NtXyAa',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+        e.target.reset()
+    };
+
+// ___________________________________________________________________________________
+// ___________________________________________________________________________________
+// ___________________________________________________________________________________
+const [messageSent, setMessageSent] = useState(false);
+
+
+
+
+
+
   return (
     <div className='ContactHome' id='Contact'>
       
@@ -37,12 +74,14 @@ const Contact = () => {
 
 <div className="ContactHomeEmallZone">
 
-<form className='ContactHomeform' action="">
+<form className='ContactHomeform' ref={form} onSubmit={sendEmail}>
     <h1>Say Hello</h1>
-    <input className='ContactHomeformData' type="text" placeholder="* Your name..." />
-    <input className='ContactHomeformData' type="email" placeholder="* Your email..." />
-    <input className='ContactHomeformComint' type="text" placeholder="* Comments..." />
-    <button className='ContactHomeformButton' id="send-mail">Send message</button>
+    <input className='ContactHomeformData' name="Name" type="text" placeholder="* Your name..." />
+    <input className='ContactHomeformData' name="Email" type="email" placeholder="* Your email..." />
+    <input className='ContactHomeformData' name="Phone" type='phone' placeholder="* Your phone..." />
+    <input className='ContactHomeformComint' name="message" type="text" placeholder="* Comments..." />
+    <button className={messageSent? 'messageSentNO':'ContactHomeformButton'} onClick={()=>{setMessageSent(true)}} type="submit"  id="send-mail">Send message</button>
+    <h2 className={messageSent? 'messageSentOK':'messageSentNO'}>Message sent successfully</h2>
 </form>
 
 </div>
@@ -54,13 +93,13 @@ const Contact = () => {
     <p className='ContactHomeEndZonePhone'>+201207604778</p>
     <h3 className='ContactHomeEndZoneTEXT'>We Are Social</h3>
     <div className="ContactHomeEndsoshelLeinks">
-        <Link href='/' className='ContactHomeEndsoshelLeink'><FaFacebookF /></Link>
-        <Link href='/' className='ContactHomeEndsoshelLeink'><FaTwitter /></Link>
-        <Link href='/' className='ContactHomeEndsoshelLeink'><FaInstagram /></Link>
-        <Link href='/' className='ContactHomeEndsoshelLeink'><FaLinkedinIn /></Link>
-        <Link href='/' className='ContactHomeEndsoshelLeink'><FaYoutube /></Link>
-        <Link href='/' className='ContactHomeEndsoshelLeink'><FaGithub /></Link>
-        <Link href='/' className='ContactHomeEndsoshelLeink'><FaWhatsapp /></Link>
+        <Link target='blank' href='https://www.facebook.com/profile.php?id=61576986519246&notif_id=1752122826894689&notif_t=page_user_activity&ref=notif#' className='ContactHomeEndsoshelLeink'><FaFacebookF /></Link>
+        <Link target='blank' href='https://x.com/GoldenWeb99' className='ContactHomeEndsoshelLeink'><FaTwitter /></Link>
+        <Link target='blank' href='https://www.instagram.com/amr.ath.9/' className='ContactHomeEndsoshelLeink'><FaInstagram /></Link>
+        <Link target='blank' href='https://www.linkedin.com/in/amr-apozeid-4b96a7251/' className='ContactHomeEndsoshelLeink'><FaLinkedinIn /></Link>
+        <Link target='blank' href='https://www.youtube.com/?app' className='ContactHomeEndsoshelLeink'><FaYoutube /></Link>
+        <Link target='blank' href='https://github.com/amrAbozeid99' className='ContactHomeEndsoshelLeink'><FaGithub /></Link>
+        <Link target='blank' href='https://wa.me/201158392932' className='ContactHomeEndsoshelLeink'><FaWhatsapp /></Link>
     </div>
 </div>
 
